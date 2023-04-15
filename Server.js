ec1421eba3dd3b38e06787d177e1fae7d2195ea6
Kimmo Lahtinen <gimble91@hotmail.com>
2023-04-15 09:12:49 +0300

    server

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const axios = require("axios");
+
 const cors = require("cors");
 
 //create app with express
