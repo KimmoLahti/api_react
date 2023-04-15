0875c3f8e48b9b94c8177df57f89edfbf43fd075
Kimmo Lahtinen <gimble91@hotmail.com>
2023-04-15 12:07:44 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
-import Getall from "./Getall.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./Default.css";
+import Navbar from "./Navbar";
+import Getall from "./Getall.js";
 import AddData from "./AddData.js";
 import Search from "./Search.js";
 import Update from "./Update.js";
 
 function App() {
   return (
-    <div className="app-container">
-      <h1 className="app-header">Database App</h1>
-      <div className="app-content">
-        <Getall />
-        <Search />
-        <AddData />
-
-        <Update />
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Switch>
+          <Route path="/getall">
+            <Getall />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/add-data">
+            <AddData />
+          </Route>
+          <Route path="/update">
+            <Update />
+          </Route>
+          <Route path="/">
+            <h1>Welcome to the Database App!</h1>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
